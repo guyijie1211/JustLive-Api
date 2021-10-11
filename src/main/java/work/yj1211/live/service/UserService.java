d41ec559343433230544c8d2d345367733673cb6
YJ1211 <1056025931@qq.com>
2021-10-11 11:43:22 +0800

    更新接口使用蓝奏云

--- src/main/java/work/yj1211/live/service/UserService.java
+++ src/main/java/work/yj1211/live/service/UserService.java
@@ -4,14 +4,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import work.yj1211.live.mapper.UserMapper;
 import work.yj1211.live.utils.Global;
+import work.yj1211.live.utils.http.HttpContentType;
+import work.yj1211.live.utils.http.HttpRequest;
 import work.yj1211.live.vo.BanInfo;
 import work.yj1211.live.vo.UpdateInfo;
 import work.yj1211.live.vo.UserInfo;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class UserService {
+    private static final Pattern PATTERN = Pattern.compile("domianload = '([\\s\\S]*?)';");
+    private static final Pattern PATTERN2 = Pattern.compile("downloads = '([\\s\\S]*?)';");
 
     @Autowired
     private UserMapper userMapper;
@@ -49,7 +55,30 @@ public class UserService {
     }
 
     public UpdateInfo checkUpdate() {
-        return Global.updateInfo;
+        UpdateInfo updateInfo = new UpdateInfo(Global.updateInfo);
+        String real_updateUrl = getDownloadUrl(updateInfo.getUpdateUrl());
+        updateInfo.setUpdateUrl(real_updateUrl);
+        return updateInfo;
     }
 
+    public String getDownloadUrl(String shortUrl) {
+        String share_url = "https://wwe.lanzoui.com/tp/" + shortUrl;
+        String response = HttpRequest.create(share_url)
+                .setContentType(HttpContentType.FORM)
+                .putHeader("User-Agent", "Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Mobile Safari/537.36")
+                .get().getBody();
+        Matcher matcher = PATTERN.matcher(response);
+        Matcher matcher2 = PATTERN2.matcher(response);
+        if (!matcher2.find()){
+            return "";
+        }
+        if (!matcher.find()) {
+            return "";
+        }
+        String result = matcher.group();
+        String result2 = matcher2.group();
+        result = result.substring(result.indexOf("'") + 1, result.lastIndexOf("'"));
+        result2 = result2.substring(result2.indexOf("'") + 1, result2.lastIndexOf("'"));
+        return result + result2;
+    }
 }
